b8d6b8e3ff78768ff11876bdc0fbdaafa30de5f9
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-17 15:26:59 +0100

    ylamsiah

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,10 +16,10 @@ int	main(void)
 	i = 1;
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 			printf("%s", "fizzbuzz");
 		else if (i % 3 == 0)
-		    printf("%s", "fizz");
+			printf("%s", "fizz");
 		else if (i % 5 == 0)
 			printf("%s", "buzz");
 		else
