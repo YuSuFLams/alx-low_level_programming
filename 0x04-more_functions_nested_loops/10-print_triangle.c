d614be53e0868855afcea3d59c41b69e3571e275
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-17 15:31:16 +0100

    ylamsiah

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ * owned by Bwave/Bright Daniel
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+        for (k = size - i; k >= 1; k--)
+            _putchar(' ');
+        for (j = 1; j <= i; j++)
+            _putchar('#');
+        _putchar('\n');
+	}
+}
