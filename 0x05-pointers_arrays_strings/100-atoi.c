aa6bacd01d1ecd6a1e3b672a32a36ce6abf85685
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-21 15:01:31 +0100

    ylamsiah

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,17 +7,15 @@
  */
 int	_atoi(char *s)
 {
-	unsigned int	n;
-	int				sign;
+	unsigned int n = 0;
+	int sign = 1;
 
-	n = 0;
-	sign = 1;
 	while (*s)
 	{
 		if (*s == '-')
-			sign *= (-1);
+			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			n = n * 10 + *s - '0';
+			n = n * 10 + (*s - '0');
 		else if (n > 0)
 			break;
 		s++;
