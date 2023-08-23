416947e4dd5ab50ec671cf4e66326a47c701b855
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-23 16:54:52 +0100

    ylamsiah

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i, tmp;
+	int i, tmp;
 
-    i = 0;
-    while (i < (n - 1) / 2)
-    {
-    	tmp = a[i];
-	    a[i] = a[n - 1 - i];
-	    a[n - 1 - i] = tmp;
-        i++;
-    }
+	i = 0;
+	while (i < (n - 1) / 2)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+		i++;
+	}
 }
