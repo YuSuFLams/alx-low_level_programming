8494a05289bc4ee03b61d08aef497743fd398d52
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-23 17:12:45 +0100

    ylamsiah

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
+void print_number(int n)
+{
+	unsigned int tmp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	tmp = n;
+	if (tmp / 10)
+		print_number(tmp / 10);
+	_putchar(tmp % 10 + '0');
+}
