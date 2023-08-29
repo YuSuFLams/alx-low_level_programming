860717db25bfca0b9d9b70a6ab8caa57606f5a1d
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-29 14:57:49 +0100

    ylamsiah

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,7 @@
  * Return: pointer to the first occurrence of the character c in the string s,
  * or NULL if the character is not found
  */
-char *_strchr(char *s, char c)
+char	*_strchr(char *s, char c)
 {
 	int	i;
 
