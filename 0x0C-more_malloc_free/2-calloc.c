4c5218b8fd363d13be8908c3133000c9029693c7
YuSuFLams <yousseflamssieh@gmail.com>
2023-09-07 21:13:14 +0100

    ylamsiah

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,13 +12,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *s;
-	unsigned int i;
+	char			*s;
+	unsigned int	i;
 
+	if (!nmemb || !size)
+		return (NULL);
 	s = (char *)malloc(nmemb * size);
 	if (!s)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (size * nmemb); i++)
 		*(s + i) = 0;
 	return (s);
 }
