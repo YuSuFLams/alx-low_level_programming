06d39c25d6b491cc4b102d1c7a9033b518c196e9
YuSuFLams <yousseflamssieh@gmail.com>
2023-09-07 21:20:52 +0100

    ylamsiah

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,39 +9,37 @@
  *
  * Return: If the function fails, it should return NULL
  */
-
-char	*string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int	len1, len2, i;
-	char			*s;
+	unsigned int len1, len2, i;
+	char *s;
 
-	if (!s1)
+	if (s1 == NULL)
 	{
 		len1 = 0;
 	}
 	else
 	{
-		len1 = ft_strlen(s1);
+		len1 = strlen(s1);
 	}
-	if (!s1)
+	if (s2 == NULL)
 	{
 		len2 = 0;
 	}
 	else
 	{
-		len2 = ft_strlen(s2);
+		len2 = strlen(s2);
 	}
-	if (n > len2)
-	{
-		n = len2;
-	}
+	if (len2 > n)
+		len2 = n;
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (!s)
+	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		*(s + i) = *(s1 + i);
 	for (i = 0; i < len2; i++)
-		*(s + len1 + i) = *(s2 + i);
-	*(s + len1 + i) = '\0';
+		*(s + i) = *(s2 + i);
+	*(s + len1 + len2) = '\0';
 	return (s);
 }
+
