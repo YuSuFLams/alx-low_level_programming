ade979c7548b66fbf0db771b31afa2a62ba1b5b4
ylamsiah <yousseflamssieh@gmail.com>
2023-09-22 01:36:22 +0100

    ok

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,9 +7,8 @@
  * Return: no return.
  */
 
-
-void before(void)
+void    before(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
