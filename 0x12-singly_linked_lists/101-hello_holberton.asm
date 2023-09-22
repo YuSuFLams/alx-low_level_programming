b9c6b96f8c120bed7c286af952d4a27f52811f6a
ylamsiah <yousseflamssieh@gmail.com>
2023-09-22 01:34:18 +0100

    ok

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello db "Hello, Holberton",10  ; The string to be printed, followed by a newline character (10)
+    fmt db "%s",0  ; The format string for printf, terminated with a null byte
 
 section .text
     global main
@@ -7,7 +8,8 @@ section .text
 
 main:
     sub rsp, 8   ; Align the stack to 16 bytes (required by the ABI)
-    mov rdi, hello  ; Load the address of the string into rdi (1st argument for printf)
+    mov rdi, fmt  ; Load the address of the format string into rdi (1st argument for printf)
+    mov rsi, hello  ; Load the address of the string into rsi (2nd argument for printf)
     call printf  ; Call the printf function
 
     add rsp, 8   ; Restore the stack
