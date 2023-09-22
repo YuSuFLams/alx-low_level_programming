c75da06f1a047db0ac617421b65c6830a3173397
ylamsiah <yousseflamssieh@gmail.com>
2023-09-22 01:42:03 +0100

    ok

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,15 @@
-section .data
-	hello db "Hello, Holberton",10  ; The string to be printed, followed by a newline character (10)
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-	global main
+	SECTION .text
 	extern printf
-
+	global main
 main:
-	sub rsp, 8   ; Align the stack to 16 bytes (required by the ABI)
-	mov rdi, hello  ; Load the address of the string into rdi (1st argument for printf)
-	call printf  ; Call the printf function
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	add rsp, 8   ; Restore the stack
-	ret          ; Return from main
+	mov eax, 0
+	ret
