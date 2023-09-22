630944909942c9da95ebb0ec7da50aa0e97693e2
ylamsiah <yousseflamssieh@gmail.com>
2023-09-22 01:32:17 +0100

    ok

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 
 void __attribute__((constructor)) premain_1(void);
 /**
- * premain - prints a string before the main function is executed
+ * premain_1 - prints a string before the main function is executed
  */
 
 void    premain_1(void)
