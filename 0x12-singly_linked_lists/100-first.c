a23cbcb823d516aaa1a184df0d8949fa6fcf2d26
ylamsiah <yousseflamssieh@gmail.com>
2023-09-22 01:32:56 +0100

    ok

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,5 @@ void __attribute__((constructor)) premain_1(void);
 
 void    premain_1(void)
 {
-    printf("You're beat! and yet, you must allow,\n"
-    "I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
