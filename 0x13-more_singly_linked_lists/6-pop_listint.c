3f355893f30f6b25b6e76f0f2c6378101afb4936
ylamsiah <yousseflamssieh@gmail.com>
2023-09-26 17:32:28 +0100

    ylamsiah

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,14 @@
 
 int pop_listint(listint_t **head)
 {
-    listint_t	*lst;
-    int			cont;
+	listint_t *temp;
+	int num;
 
-    if (!(*head) || !head)
-        return (0);
-    lst = *head;
-    cont = lst->n;
-    free(lst);
-    *head = (*head)->next;
-    return (cont);
+	if (!head || !*head)
+		return (0);
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (num);
 }
