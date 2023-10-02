8ed5242267d338c398cf964a24b1273d7bd60797
ylamsiah <yousseflamssieh@gmail.com>
2023-10-02 23:10:50 +0100

    ylamsiah

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,12 +14,17 @@ ssize_t	read_textfile(const char *filename, size_t letters)
 	ssize_t	s;
 	ssize_t	count;
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer || !filename)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (free(buffer), 0);
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
 	s = read(fd, buffer, letters);
 	if (s == -1)
 		return (free(buffer), close(fd), 0);
