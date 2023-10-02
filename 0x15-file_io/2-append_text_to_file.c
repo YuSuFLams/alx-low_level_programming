ed918484d7d1a0dcc55bc45e15add3c82947ceaa
ylamsiah <yousseflamssieh@gmail.com>
2023-10-02 23:07:59 +0100

    ls

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int	append_text_to_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len]; len++)
 		;
 	count = write(fd, text_content, len);
-	if (count < 0)
+	if (count == -1)
 		return (close(fd), -1);
 	return (close(fd), 1);
 }
