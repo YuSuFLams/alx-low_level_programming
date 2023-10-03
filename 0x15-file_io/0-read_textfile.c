5db12b4579dfb9ed02274a7fa6ff08138ce577cc
ylamsiah <yousseflamssieh@gmail.com>
2023-10-03 01:04:13 +0100

    ok

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,44 @@
 #include "main.h"
 
 /**
- * read_textfile - a function that reads a text file and prints it to the POSIX standard output.
+ * read_textfile - a function that reads a text file and prints it to the POSIX standard count.
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
  * Return: number of letters it could read and print
  */
 
-ssize_t	read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int		fd;
-	char	*buffer;
-	ssize_t	count, s;
+	int fd = -1;
+	ssize_t count = 0;
+	char *buffer;
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer || !filename )
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (free(buffer), 0);
+	if (fd < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
 
 	count = read(fd, buffer, letters);
-	if (count == -1)
-		return (free(buffer), close(fd), 0);
+	if (count < 0)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	s = write(STDOUT_FILENO, buffer, count);
-	if (s == -1)
-		return (free(buffer), close(fd), 0);
-
-	return (free(buffer), close(fd), s);
+	count = write(STDOUT_FILENO, buffer, count);
+	free(buffer);
+	close(fd);
+	if (count < 0)
+		return (0);
+	return (count);
 }
