b4340a469180b11a74aa0e6c746b7a9f98130717
ylamsiah <yousseflamssieh@gmail.com>
2023-10-03 01:01:38 +0100

    ok

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,30 +10,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t count, s;
 	char *buffer;
+	ssize_t count, s;
 
-	if (filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (!fd == -1)
-		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-	{
-		close(fd);
+	if (!buffer || !filename )
 		return (0);
-	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (free(buffer), 0);
+
 	count = read(fd, buffer, letters);
-	close(fd);
 	if (count == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+		return (free(buffer), close(fd), 0);
+
 	s = write(STDOUT_FILENO, buffer, count);
-	free(buffer);
-	if (count != s)
-		return (0);
-	return (s);
+	if (s == -1)
+		return (free(buffer), close(fd), 0);
+
+	return (free(buffer), close(fd), s);
 }
+
