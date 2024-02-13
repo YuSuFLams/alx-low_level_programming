cb2c09faa97304f358e0330f13a0644a812b6964
YuSuFLams <yousseflamssieh@gmail.com>
2024-02-13 09:38:19 +0100

    Ya Allah

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,43 +1,36 @@
 #include "main.h"
 
 /**
- * main - entry point
- * description: copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 97-100 on failure
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file to append the text.
+ * @text_content: the NULL terminated string to add at the end of the file.
+ * Return: 1 on success, -1 on failure
  */
-int main(int argc, char *argv[])
+
+int	append_text_to_file(const char *filename, char *text_content)
 {
-	int fd_in, fd_out, count, s;
-	char buf[1024];
+	int	fd;
+	int	len;
+	int	count;
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
-	/* open file_from */
-	fd_in = open(argv[1], O_RDONLY);
-	if (fd_in == -1)
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		close(fd);
+		return (1);
 	}
-
-	/* open file_to */
-	fd_out = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_out == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-	/* read from file_from and write to file_to */
-	while ((count = read(fd_in, buf, 1024)) > 0)
-	{
-		s = write(fd_out, buf, count);
-		if (s == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-
+	for (len = 0; text_content[len]; len++)
+		;
+	count = write(fd, text_content, len);
 	if (count == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		close(fd);
+		return (-1);
 	}
+	close(fd);
+	return (1);
+}
